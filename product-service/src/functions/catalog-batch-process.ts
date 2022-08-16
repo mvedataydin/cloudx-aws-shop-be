4dbd79d273ee2d14cbd6864c0f6dab8073a4088a
mvedataydin <mvedataydin@gmail.com>
2022-08-16 22:06:08 +0300

    feat(task-6): log sns notification details on catalogBatchProcess lambda service

--- product-service/src/functions/catalog-batch-process.ts
+++ product-service/src/functions/catalog-batch-process.ts
@@ -2,6 +2,7 @@ import { lambdaHandler } from '../utils/handler.utils';
 import * as productService from '../services/product.service';
 import { SQSEvent } from 'aws-lambda';
 import { PublishCommand, SNSClient } from '@aws-sdk/client-sns';
+import logger from '@utils/logger.utils';
 
 export const catalogBatchProcess = lambdaHandler(async (event: SQSEvent) => {
   const snsClient = new SNSClient({ region: 'eu-west-1' });
@@ -10,17 +11,26 @@ export const catalogBatchProcess = lambdaHandler(async (event: SQSEvent) => {
   for (const record of records) {
     const product = await productService.createProduct(record);
     if (product) {
-      const input = {
-        Subject: 'New product added to Database',
-        Message: JSON.stringify(product),
-        TopicArn: process.env.SNS_TOPIC_ARN,
-        MessageAttributes: {
-          overstock: {
-            DataType: 'String',
-            StringValue: +product.count >= 100 ? 'true' : 'false',
-          },
+      const message = JSON.stringify(product);
+      const messageAttributes = {
+        overstock: {
+          DataType: 'String',
+          StringValue: +product.count >= 100 ? 'true' : 'false',
         },
       };
+      const input = {
+        Subject: 'New product added to Database',
+        Message: message,
+        TopicArn: process.env.SNS_TOPIC_ARN,
+        MessageAttributes: messageAttributes,
+      };
+
+      logger.log(
+        'Sending notification:',
+        message,
+        'with attributes:',
+        messageAttributes,
+      );
 
       const command = new PublishCommand(input);
       await snsClient.send(command);
