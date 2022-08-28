f5c395ac93f43c707bb9d702582599a7703cacc2
mvedataydin <mvedataydin@gmail.com>
2022-08-28 20:18:57 +0300

    feat(task-7): add  authorizer to import-products-file lambda function

--- import-service/src/functions/import-products-file/index.ts
+++ import-service/src/functions/import-products-file/index.ts
@@ -17,6 +17,12 @@ export default {
             },
           },
         },
+        authorizer: {
+          name: 'tokenAuthorizer',
+          arn: 'arn:aws:lambda:${self:provider.region}:${aws:accountId}:function:authorization-service-dev-basicAuthorizer',
+          identitySource: 'method.request.header.Authorization',
+          type: 'token',
+        },
       },
     },
   ],
